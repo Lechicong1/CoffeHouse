b71999885c083a084d254516a21425519585b4bf
Nguyen Viet Quang <quangsts123@outlook.com>
2026-01-09 19:38:53 +0700

    sửa employee

--- web/Views/AdminDashBoard/Pages/Employees_v.php
+++ web/Views/AdminDashBoard/Pages/Employees_v.php
@@ -72,7 +72,7 @@ $roles = $data['roles'] ?? [
                     <th>Email</th>
                     <th>Số điện thoại</th>
                     <th>Lương</th>
-                    <th>Thao tác</th>
+                    <th>Ngày vào làm</th>
                 </tr>
             </thead>
             <tbody>
